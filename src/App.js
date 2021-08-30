fe008732566d6f9b72416e88db3ea89e7da7cd6b
Lasha Jamaspashvili <ljamaspashvili@gmail.com>
2021-08-30 12:33:08 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import Lasha from './components/Lasha';
+import { DUMMY_DATA } from './components/Lasha';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Lasha />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+// const LashaJam = () => <h1>Lasha Jam</h1>;
---
+++ src/components/Lasha.js
@@ -0,0 +1,13 @@
+export const DUMMY_DATA = [
+  { name: 'Lasha', surname: 'Jamaspashvili' },
+  { name: 'L', surname: 'J' },
+];
+
+
+const Lasha = () => {
+  return (
+    <h1>Lasha Jamaspashvili</h1>
+  )
+};
+
+export default Lasha;
